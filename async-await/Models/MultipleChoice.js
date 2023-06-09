4d99fdd8f0c963afe9460b3bc4763328be7b2683
ducviethuynh <huynhvietduchvd@gmail.com>
2023-06-09 23:03:54 +0700

    new file:   async-await/Models/FillInBlank.js new file:   async-await/Models/MultipleChoice.js new file:   async-await/Models/Question.js new file:   async-await/index.html new file:   async-await/script/main.js

---
+++ async-await/Models/MultipleChoice.js
@@ -0,0 +1,11 @@
+class MultipleChoice extends Question {
+  constructor(type, id, content, answer) {
+    super(type, id, content, answer);
+  }
+
+  render() {
+    return `
+      
+    `;
+  }
+}
